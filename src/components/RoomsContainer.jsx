1fff6074e478f79f4d956819a109adf3cd986d62
Christian Dimitrov <christian_dimitrov@abv.bg>
2019-09-28 23:30:48 +0300

    helper components for rooms page added. styled component for dynamic  added

---
+++ src/components/RoomsContainer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import RoomsFilter from "./RoomsFilter";
+import RoomsList from "./RoomsList";
+import { RoomConsumer } from "../context";
+import Loading from "./Loading";
+
+export default function RoomsContainer() {
+  return (
+    <RoomConsumer>
+      {value => {
+        const { loading, sortedRooms, rooms } = value;
+        if (loading) {
+          return <Loading />;
+        }
+        return (
+          <>
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+          </>
+        );
+      }}
+    </RoomConsumer>
+  );
+}
